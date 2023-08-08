c975afd4b6490fc0029fe01f34b32b3a893adddc
Valery Chernov <valery.chernov@deelvin.com>
2023-08-08 17:32:11 +0400

    add TVMModel class

--- lmentry/relax_model_wrapper.py
+++ lmentry/relax_model_wrapper.py
@@ -1,7 +1,68 @@
+import os
 import argparse
-from typing import Callable
+from typing import Callable, List
 
 import torch
+import tvm
+from tvm import relax
+from transformers import AutoTokenizer
+
+
+def load_params(artifact_path: str, device) -> List[tvm.nd.NDArray]:
+  from tvm.contrib import tvmjs  # pylint: disable=import-outside-toplevel
+
+  params, meta = tvmjs.load_ndarray_cache(f"{artifact_path}/params", device)
+  plist = []
+  size = meta["ParamSize"]
+  for i in range(size):
+      plist.append(params[f"param_{i}"])
+  return plist
+
+
+class TVMModel:
+  def __init__(self, args) -> None:
+    self.device = tvm.device(args.device_name)
+    self.const_params = load_params(args.artifact_path, self.device)
+    ex = tvm.runtime.load_module(
+      os.path.join(
+        args.artifact_path,
+        # TODO(vchernov): check input args, refactor to json config
+        f"{args.model}-{args.quantization.name}-{args.device_name}.so",
+      )
+    )
+    self.vm = relax.VirtualMachine(ex, self.device)
+
+    self.tot_seq_len = 0
+    self.kv_cache = self.vm["create_kv_cache"]()
+    self.args = args
+    try:
+      self.prefill_func = self.vm["prefill"]
+    except AttributeError:
+      self.prefill_func = None
+
+  def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+    inputs = inputs.numpy()
+    self.tot_seq_len += inputs.shape[1]
+    seq_len_shape = tvm.runtime.ShapeTuple([self.tot_seq_len])
+    if inputs.shape[1] > 1 and self.prefill_func:
+      inputs = tvm.nd.array(inputs.numpy(), device=self.device)
+      logits, kv_cache = self.prefill_func(
+          inputs, seq_len_shape, self.kv_cache, self.const_params
+      )
+    else:
+      for i in range(inputs.shape[1]):
+        input_slice = tvm.nd.array(inputs[:, i : i + 1], device=self.device)
+        logits, kv_cache = self.vm["decode"](
+            input_slice, seq_len_shape, self.kv_cache, self.const_params
+        )
+    self.kv_cache = kv_cache
+
+    return torch.from_numpy(logits.numpy())
+
+
+def get_tvm_model(args):
+    model = TVMModel(args)
+    return model.forward
 
 
 def sample_top_p(probs, p):
@@ -45,12 +106,6 @@ class RelaxModelWrapper:
       else:
         logits = self.model(tokens[:, cur_pos - 1 : cur_pos])
       logits = logits[:, -1, :].to(torch.float64)
-      np_logits = logits.detach().cpu().numpy().astype("float64")
-      if self.args.debug_dump:
-        print(
-          f"logits: min = {np_logits.min()}, max = {np_logits.max()}, "
-          f"mean = {np_logits.mean()}, std = {np_logits.std()}",
-        )
       if self.temperature > 0:
         probs = torch.softmax(logits / self.temperature, dim=-1)
         next_token = sample_top_p(probs, self.top_p)
@@ -63,3 +118,14 @@ class RelaxModelWrapper:
         break
 
     return tokens[:, :cur_pos + 1]
+
+
+def get_relax_model(args):
+  tokenizer = AutoTokenizer.from_pretrained(
+    os.path.join(args.artifact_path, "params"), trust_remote_code=True
+  )
+  tokenizer.pad_token_id = tokenizer.eos_token_id
+  if args.model.startswith("dolly-"):
+      # 50277 means "### End"
+      tokenizer.eos_token_id = 50277
+  return RelaxModelWrapper(get_tvm_model(args), tokenizer, args)
