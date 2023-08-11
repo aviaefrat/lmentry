20ee4e151cbc43e045f19f682e80f3e448f422b4
Valery Chernov <valery.chernov@deelvin.com>
2023-08-11 14:01:54 +0400

    add quantization tag to model name to split it in statistics

--- lmentry/model_manager.py
+++ lmentry/model_manager.py
@@ -27,8 +27,9 @@ class ModelManager:
         mlc_config = json.load(json_file)
 
       model_local_id = mlc_config["local_id"]
-      model_name_and_quant_list = model_local_id.split("-")
-      self.model_name = "".join(model_name_and_quant_list[:-1])
+      self.model_name = model_local_id
+      # model_name_and_quant_list = model_local_id.split("-")
+      # self.model_name = "".join(model_name_and_quant_list[:-1])
 
       self.config ={
         "artifact_path": model_name,
