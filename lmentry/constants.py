4afcacad67f2abd88110dea7c39a6c23df04a8ff
Valery Chernov <valery.chernov@deelvin.com>
2023-08-14 12:16:32 +0400

    update gitignore

--- lmentry/constants.py
+++ lmentry/constants.py
@@ -37,7 +37,6 @@ paper_models = {
 hf_models = {
     "vicuna-7b-v1-3": {"short_name": "vicuna", "paper_name": "Vicuna-7b", "predictor_name": "lmsys/vicuna-7b-v1.3"},
 # TODO(vchernov): workaround for mlc-models short names
-    "vicuna7bv1": {"short_name": "vicuna7bv1"},
     "vicuna-7b-v1-3-q4f16_0": {"short_name": "vicuna-7b-v1-3-q4f16_0"},
     "vicuna-7b-v1-3-q0f16": {"short_name": "vicuna-7b-v1-3-q0f16"},
 }
