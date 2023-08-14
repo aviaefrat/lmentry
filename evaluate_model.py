93dce30b4a3bc0115e92af6a100f472c4bcda912
Valery Chernov <valery.chernov@deelvin.com>
2023-08-14 16:22:51 +0400

    small clean

--- evaluate_model.py
+++ evaluate_model.py
@@ -18,10 +18,10 @@ def parse_arguments():
   parser.add_argument("-m", "--model_names", nargs="+", type=str, default=None,
                       help="Names of models which statistics were collected and should evaluate. "
                            "If it is None the predictions directory is analized and evailable model-statistics are evaluated")
-  parser.add_argument('-t', '--task_name', nargs="+", type=str, default=None,
-                      help=f"If need to evaluate specified set of tasks set their names. "
-                            "Task names should be from the list: {all_tasks.keys()}. "
-                            "It tries to analyze all tasks by default")
+  parser.add_argument('-t', '--task_names', nargs="+", type=str, default=None,
+                      help="If need to evaluate specified set of tasks set their names. "
+                           f"Task names should be from the list: {all_tasks.keys()}. "
+                           "It tries to analyze all tasks by default")
   parser.add_argument("-n", "--num-procs",
                       default=1,
                       type=int,
@@ -40,8 +40,8 @@ def main():
   RESULTS_DIR.mkdir(exist_ok=True)
 
   args = parse_arguments()
-  if args.task_name is not None:
-    task_names = args.task_name
+  if args.task_names is not None:
+    task_names = args.task_names
   else:
     task_names = sorted(all_tasks.keys())
   # model_names = ["vicuna-7b-v1-3", "vicuna-7b-v1-3-q0f16", "vicuna-7b-v1-3-q4f16_0"] # HF, mlc_q0f16, mlc_q4f16_0
