cc2ea317ed81890680d329fb91819676c1c71e66
Valery Chernov <valery.chernov@deelvin.com>
2023-10-04 10:06:04 +0400

    add capital task for Russian

--- lmentry/scorers/simple_tasks_scorers.py
+++ lmentry/scorers/simple_tasks_scorers.py
@@ -37,3 +37,41 @@ class CapitalScorer(LMentryScorer):
 
     score, certainty = self.certainty_scorer(prediction, base_patterns)
     return score, certainty
+
+
+class CapitalRuScorer(LMentryScorer):
+  def __init__(self):
+    super().__init__()
+
+  def get_base_patterns(self, country, answer):
+    base_patterns = [
+        rf"{answer} столица {country}",
+        rf"Столица {country} {answer}",
+    ]
+
+    return base_patterns + self.get_shared_patterns_ru(target=answer)
+
+  def score_prediction(self, prediction, example, truncate_prediction: bool = False):
+    prediction = self.normalize_prediction(prediction, truncate_prediction)
+
+    metadata = example["metadata"]
+    answer = metadata["answer"]
+
+    score, certainty = self.negative_scorer(prediction, answer)
+    if score is not None:
+        return score, certainty
+
+    country = metadata["country"]
+
+    # TODO(vvchernov): check many words and use the_words_regex
+    answer = the_word_regex(answer)
+    country = the_word_regex(country)
+
+    score, certainty = self._simple_scorer(prediction, answer)
+    if score:
+        return score, certainty
+
+    base_patterns = self.get_base_patterns(country, answer)
+
+    score, certainty = self.certainty_scorer(prediction, base_patterns)
+    return score, certainty
